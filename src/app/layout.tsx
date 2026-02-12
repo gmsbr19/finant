69ed69719bafde8034344b748094a0a2ebb0a4a3
Thiago <thiagovidaportela@gmail.com>
2026-02-12 19:47:40 -0300

    feat: added Geist font family

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,25 @@
-import type { Metadata } from "next";
+import type { Metadata } from "next"
+import { GeistSans } from "geist/font/sans"
+import { GeistMono } from "geist/font/mono"
 
 import "@/styles/global.css"
 
 export const metadata: Metadata = {
-  title: "Finant",
-  description: "Gestão financeira pessoal",
-};
+    title: "Finant",
+    description: "Gestão financeira pessoal",
+}
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode
 }>) {
-  return (
-    <html lang="pt-BR">
-      <body>
-        {children}
-      </body>
-    </html>
-  );
+    return (
+        <html
+            lang="pt-BR"
+            className={`${GeistSans.variable} ${GeistMono.variable}`}
+        >
+            <body>{children}</body>
+        </html>
+    )
 }
