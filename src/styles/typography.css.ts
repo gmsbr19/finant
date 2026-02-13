429fcd2c276416a469e742149587fd6e349eda5e
Thiago <thiagovidaportela@gmail.com>
2026-02-13 20:28:22 -0300

    refactor: base styles for typography and containers

---
+++ src/styles/typography.css.ts
@@ -0,0 +1,38 @@
+import { style } from "@vanilla-extract/css"
+import { vars } from "./theme.css"
+
+const body = style({
+    fontStyle: "normal",
+    fontWeight: vars.fontWeights.medium,
+    lineHeight: vars.lineHeights.body,
+})
+
+export const bodyS = style([
+    body,
+    {
+        fontSize: vars.fontSizes.bodyS
+    }
+])
+
+export const bodyM = style([
+    body,
+    {
+        fontSize: vars.fontSizes.bodyM,
+    },
+])
+
+export const moneyL = style({
+    fontFamily: vars.fonts.mono,
+    color: vars.colors.textPrimary,
+    fontSize: vars.fontSizes.h1,
+    fontStyle: "normal",
+    fontWeight: vars.fontWeights.semibold,
+    lineHeight: "normal",
+})
+
+export const moneyM = style([
+    moneyL,
+    {
+        fontSize: vars.fontSizes.h3,
+    },
+])
