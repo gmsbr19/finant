05b9a41255c39c34e0c19b261948c0fe3b647a10
Thiago <thiagovidaportela@gmail.com>
2026-02-14 17:48:34 -0300

    refactor: created function for month date interval

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,11 +1,21 @@
 export function getTransactionVariant(t: any) {
-  if (t.type === 'INCOME') return 'income';
-  
-  const mainCategoryName = t.category?.parent?.name || t.category?.name || '';
+    if (t.type === "INCOME") return "income"
 
-  if (mainCategoryName.includes('[S]')) return 'survival';
-  if (mainCategoryName.includes('[L]')) return 'eudaimonia';
-  if (mainCategoryName.includes('[R]')) return 'resilience';
-  
-  return 'transfer';
+    const mainCategoryName = t.category?.parent?.name || t.category?.name || ""
+
+    if (mainCategoryName.includes("[S]")) return "survival"
+    if (mainCategoryName.includes("[L]")) return "eudaimonia"
+    if (mainCategoryName.includes("[R]")) return "resilience"
+
+    return "transfer"
+}
+
+export function getMonthDateInterval(
+    year: number,
+    month: number,
+): { start: Date; end: Date } {
+    const start = new Date(Date.UTC(year, month - 1, 1))
+    const end = new Date(Date.UTC(year, month, 1))
+
+    return { start, end }
 }
