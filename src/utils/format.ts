33dc15dfdf31cf06201f9e1d466138399b50f1fe
Thiago <thiagovidaportela@gmail.com>
2026-02-14 12:29:05 -0300

    refactor: changed formatDateLabel location

--- src/utils/format.ts
+++ src/utils/format.ts
@@ -11,3 +11,15 @@ export function formatCurrency(value: number | string | Decimal) {
         currency: "BRL",
     }).format(amount)
 }
+
+export function formatDateLabel(dateStr: string) {
+    const date = new Date(dateStr)
+    date.setMinutes(date.getMinutes() + date.getTimezoneOffset())
+
+    const today = new Date()
+    const isToday = date.toDateString() === today.toDateString()
+
+    if (isToday) return "Hoje"
+
+    return date.toLocaleDateString("pt-BR", { day: "2-digit", month: "long" })
+}
