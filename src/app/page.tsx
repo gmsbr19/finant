e0f08dd3f4fe46e2c61c9369e7470e60bc4acd64
Thiago <thiagovidaportela@gmail.com>
2026-02-16 12:16:47 -0300

    feat: add getAccountsNamesAndIds service and integrate accounts into TransactionForm

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,16 @@ import TransactionForm from "@/features/data/TransactionForm"
 import * as styles from "./page.css"
 import { Home } from "@/features/home"
 import { getCategoriesNamesAndIds } from "@/services/categories"
+import { getAccountsNamesAndIds } from "@/services/accounts"
 
 export default async function Page() {
     const categories = await getCategoriesNamesAndIds()
+    const accounts = await getAccountsNamesAndIds()
 
     return (
         <div className={styles.background}>
             <Home />
-            <TransactionForm categories={categories} />
+            <TransactionForm categories={categories} accounts={accounts} />
         </div>
     )
 }
