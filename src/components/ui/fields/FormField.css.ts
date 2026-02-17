00e2a4e743ad803b64ba511d477f62d59d72cef6
Thiago <thiagovidaportela@gmail.com>
2026-02-17 20:27:55 -0300

    refactor: streamline FormField styles and remove unused variables

--- src/components/ui/fields/FormField.css.ts
+++ src/components/ui/fields/FormField.css.ts
@@ -1,43 +1,54 @@
-import { colFlexContainer } from "@/styles/containers.css"
 import { vars } from "@/styles/theme.css"
-import { style } from "@vanilla-extract/css"
+import { bodyM, bodyS } from "@/styles/typography.css"
+import { createVar, style } from "@vanilla-extract/css"
 
-const background = style({
-    backgroundColor: vars.colors.bgBase,
-    border: `1px solid ${vars.colors.border}`,
-    borderRadius: vars.radii.button,
-    padding: vars.space.s,
-    width: "fit-content",
+export const colSpan = createVar()
+
+export const formFieldContainer = style({
+    display: "flex",
     height: "fit-content",
+    flexDirection: "column",
+    alignItems: "flex-start",
+    gridColumn: colSpan,
+    width: '100%'
 })
 
-export const fieldContainer = style([
-    colFlexContainer,
-    {
-        padding: vars.space.s,
-        gap: 4,
-    },
-])
+export const label = style([bodyS, { color: vars.colors.textSecondary }])
 
-export const label = style([
-    fieldContainer,
-    {
-        color: vars.colors.textSecondary,
-        fontSize: vars.fontSizes.bodyS,
-        alignSelf: 'end'
-    },
-])
+export const iconInputContainer = style({
+    display: "flex",
+    alignItems: "center",
+    gap: 4,
+    padding: 4,
+    minWidth: 'unset',
+    width: "100%",
 
-export const inputField = style([
-    background,
-    {
-        height: "42px",
-        width: "100%",
-        color: vars.colors.textPrimary,
+    backgroundColor: vars.colors.bgSurface,
+    border: `1px solid ${vars.colors.border}`,
+    borderRadius: vars.radii.button,
+})
 
-        ":focus": {
-            outline: "none",
-            boxShadow: `0 0 5px 2px ${vars.colors.border}`,
-        },
+export const iconContainer = style({
+    display: "flex",
+    padding: 4,
+    color: vars.colors.textSecondary
+})
+
+export const input = style([bodyM, {
+    display: "flex",
+    padding: "4px 8px",
+    color: vars.colors.textPrimary,
+    borderRadius: 4,
+    outline: 'none',
+    width: "100%",
+
+    ":enabled": {
+        border: `1px solid ${vars.colors.border}`,
+        backgroundColor: vars.colors.bgBase,
     },
-])
+
+    ":disabled": {
+        border: "1px solid transparent",
+        backgroundColor: "transparent",
+    },
+}])
