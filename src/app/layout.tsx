5e583f8259862236e7d2dcbd660edbf763a3e8c5
Thiago <thiagovidaportela@gmail.com>
2026-02-17 20:29:36 -0300

    feat: import globals.css for animated icons functionality

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next"
 import { GeistSans } from "geist/font/sans"
 import { GeistMono } from "geist/font/mono"
 
+import "./globals.css"
 import "@/styles/global.css"
 import NavSidebar from "@/components/navigation/NavSidebar"
 
