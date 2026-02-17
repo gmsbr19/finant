97c81a9e56ee95ca517c09f1159e8d57ec4e59dc
Thiago <thiagovidaportela@gmail.com>
2026-02-17 20:28:29 -0300

    feat: enhance FormField component with icon support and improved layout

--- src/components/ui/fields/FormField.tsx
+++ src/components/ui/fields/FormField.tsx
@@ -1,18 +1,37 @@
-import { ReactNode } from "react"
+import { ComponentProps, CSSProperties, ReactNode } from "react"
 import * as styles from "./FormField.css"
+import { LucideIcon } from "lucide-react"
+import { assignInlineVars } from "@vanilla-extract/dynamic"
 
-type FormFieldProps = {
-    label: string
+interface FormFieldProps extends ComponentProps<"label"> {
+    label?: string
+    Icon?: LucideIcon
     children: ReactNode
-    props?: React.LabelHTMLAttributes<HTMLLabelElement>
+    colSpan?: "span 1" | "span 2"
 }
 
-const FormField = ({ label, children, props }: FormFieldProps) => {
+const FormField = ({
+    label,
+    Icon,
+    children,
+    colSpan,
+    ...props
+}: FormFieldProps) => {
     return (
-        <label {...props} className={styles.label}>
-            {label}
-            {children}
-        </label>
+        <div
+            className={styles.formFieldContainer}
+            style={assignInlineVars({ [styles.colSpan]: colSpan || "span 1" }) as CSSProperties}
+        >
+            <label {...props} className={styles.label}>
+                {label}
+            </label>
+            <div className={styles.iconInputContainer}>
+                <div className={styles.iconContainer}>
+                    {Icon && <Icon size={24} />}
+                </div>
+                {children}
+            </div>
+        </div>
     )
 }
 
