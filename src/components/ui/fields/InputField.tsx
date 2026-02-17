72c227a20c9659600b6791dc17804755f34e157a
Thiago <thiagovidaportela@gmail.com>
2026-02-17 20:28:04 -0300

    fix: update InputField component to use correct class name for styling

--- src/components/ui/fields/InputField.tsx
+++ src/components/ui/fields/InputField.tsx
@@ -3,7 +3,7 @@ import * as styles from "./FormField.css"
 type InputFieldProps = React.InputHTMLAttributes<HTMLInputElement>
 
 const InputField = (props: InputFieldProps) => {
-    return <input {...props} className={styles.inputField}/>
+    return <input {...props} className={styles.input}/>
 }
 
 export default InputField
